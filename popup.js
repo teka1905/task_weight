94ee1948eef4950d270b88a41c1cc008406b1fce
a.dolmatov <a.dolmatov@2gis.ru>
2021-11-15 10:50:06 +0300

    Исправил формулу

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
 function funcCalc() {
-    let benefit = document.getElementById("benefit").value;
-    let innovation = document.getElementById("innovation").value;
-    let risk = document.getElementById("risk").value;
-    let level = document.getElementById("level").value;
+    let benefit = +document.getElementById("benefit").value;
+    let innovation = +document.getElementById("innovation").value;
+    let risk = +document.getElementById("risk").value;
+    let level = +document.getElementById("level").value;
     document.getElementById("total").innerHTML = Math.round((benefit+innovation+risk)/level)
 }
 
